dac9ae68ae8817956ebbc46fd606ab63af21b773
Andrew High <andrewhigh4@hotmail.com>
2021-04-14 17:36:22 +0100

    add page working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flast import Flask
+from flask import Flask
 
 app = Flask(__name__)
-from controllers import controllers
+from controllers import controller
 
 if __name__ == "__main__":
     app.run(debug=True)
--- controllers/controller.py
+++ controllers/controller.py
@@ -3,8 +3,29 @@ from app import app
 from modules.calculator import *
 
 @app.route("/add/<num_1>/<num_2>")
-def add_page():
+def add_page(num_1, num_2):
     num_1 = int(num_1)
     num_2 = int(num_2)
     total = add(num_1,num_2)
-    return render_template("add.html")
+    return render_template("add.html", num_1 = num_1, num_2 = num_2, total = total)
+
+@app.route("/subtract/<num_1>/<num_2>")
+def subtract_page(num_1, num_2):
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+    total = subtract(num_1,num_2)
+    return render_template("subtract.html", num_1 = num_1, num_2 = num_2, total = total)
+
+@app.route("/multiply/<num_1>/<num_2>")
+def multiply_page(num_1, num_2):
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+    total = multiply(num_1,num_2)
+    return render_template("multiply.html", num_1 = num_1, num_2 = num_2, total = total)
+
+@app.route("/divide/<num_1>/<num_2>")
+def divide_page(num_1, num_2):
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+    total = divide(num_1,num_2)
+    return render_template("divide.html", num_1 = num_1, num_2 = num_2, total = total)
